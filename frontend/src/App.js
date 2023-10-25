33e252654a094ece0ad9bd040eb3eb68f7f42db5
unknown <anjalidelhi1509@gmail.com>
2023-10-26 00:10:21 +0530

    CRUD operations done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,23 +7,48 @@ import axios from "axios"
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-
 function App() {
-  const [todo, setTodo] = useState([])
+  const [todo, setTodo] = useState([]);
+  const [text, setText] = useState("");
+  const [isUpdating, setIsUpdating] = useState(false);
+  const [updateItemId, setUpdateItemId] = useState(null);
+
+  const createTodo = async (e) => {
+    e.preventDefault();
+    try {
+      let res = await axios.post("http://localhost:5000/create-todo", { text });
+      console.log(res);
+      if (res.data.success) {
+        toast(res.data.message, {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          success: true,
+          theme: "dark",
+        });
+        getTodo();
+        setIsUpdating(false);
+        setText("");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
   const getTodo = async () => {
-    let res = await axios("http://localhost:5000/get-todo")
-
-    console.log(res.data.todo)
-    setTodo(res.data.todo)
+    let res = await axios.get("http://localhost:5000/get-todo");
+    console.log(res.data.todo);
+    setTodo(res.data.todo);
   }
 
   useEffect(() => {
-    getTodo()
-  }, [])
+    getTodo();
+  }, []);
 
   const handleDelete = async (id) => {
-    console.log(id);
     try {
       const res = await axios.delete("http://localhost:5000/delete-todo", {
         data: { id }
@@ -39,21 +64,52 @@ function App() {
     }
   };
 
-
-
-  const handleUpdate = () => {
-
+  const updateMode = (id, text) => {
+    console.log("Update mode activated for ID:", id);
+    setIsUpdating(true);
+    setUpdateItemId(id);
+    setText(text);
+    console.log(text)
   }
+
+  const handleUpdate = async () => {
+    try {
+      const res = await axios.put("http://localhost:5000/update-todo", {
+        id: updateItemId,
+        text,
+      });
+      if (res.data.success) {
+        toast(res.data.message);
+        getTodo();
+        setIsUpdating(false);
+        setText("");
+      } else {
+        toast(res.data.err);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
     <div className="App">
       <div className='container'>
-        <TodoInput />
+        <div className="todo-container">
+          <h1>TODO</h1>
+          <input value={text} onChange={(e) => setText(e.target.value)} type="text" placeholder="Add Todos..." />
+          <button onClick={isUpdating ? handleUpdate : createTodo} className="add-btn">
+            {isUpdating ? "Update" : "Add"}
+          </button>
+        </div>
         {
           todo.map((item) =>
-            <Todo text={item.text} handleUpdate={handleUpdate} handleDelete={() => handleDelete(item._id)} />
+            <Todo
+              text={item.text}
+              handleUpdate={() => updateMode(item._id, item.text)}
+              handleDelete={() => handleDelete(item._id)}
+            />
           )
         }
-
       </div>
       <ToastContainer
         position="top-right"
